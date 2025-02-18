96005fcaacc38d7f6fecfe4fd77428251d367e60
Guilherme M L Furlaneto <55238444+GMLF@users.noreply.github.com>
2025-02-18 10:48:14 -0300

    Delete ProjetoFinal/projetoFinal directory

--- ProjetoFinal/projetoFinal/src/HOR.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface HOR extends Remote {
-    public String getHOR() throws RemoteException;
-
-    public void setHOR(String valor) throws RemoteException;
-
-}
